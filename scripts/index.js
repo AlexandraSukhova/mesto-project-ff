8ba46b21d365712dcf423b85e4c7d03f1d7d0b9a
Alexandra Sukhova <sukhova_alexandra@mail.ru>
2024-03-11 16:46:00 +0300

    Первая отправка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const cardList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function cardCreate(card, cardDelete) {
+  const cardItem = cardTemplate.querySelector(".places__item").cloneNode(true); // Обязательно объявлять и клонировать здесь
+  const cardImage = cardItem.querySelector(".card__image");
+  const cardTitle = cardItem.querySelector(".card__title");
 
+  cardImage.src = card.link;
+  cardImage.alt = card.alt;
+  cardTitle.textContent = card.name;
+
+  const cardDeleteButton = cardItem.querySelector(".card__delete-button");
+  cardDeleteButton.addEventListener("click", cardDelete(cardDeleteButton));
+  return cardItem;
+}
 // @todo: Функция удаления карточки
-
+function cardDelete(button) {
+  button.addEventListener("click", function(evt){
+    const eventTarget = evt.target;
+    eventTarget.closest(".places__item").remove();
+  });
+}
 // @todo: Вывести карточки на страницу
+function showCards() {
+ initialCards.forEach(card => 
+  cardList.append(cardCreate(card, cardDelete))
+  );
+}
+showCards();
+
